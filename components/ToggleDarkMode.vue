e6439d1d8db77d80dc403e525b085ef564f0c24c
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-06-22 17:44:28 +0800

    Style: Minor changes

--- components/ToggleDarkMode.vue
+++ components/ToggleDarkMode.vue
@@ -20,14 +20,11 @@ const isDarkMode = useDark({ ...props?.options, disableTransition: false });
 <template>
   <AppFormCheckbox
     v-model="isDarkMode"
-    :label="isDarkMode ? 'Dark' : 'Light'"
-    label-class="hidden md:block"
   >
     <template #checkbox="{ isActive, onChangeHandler }">
       <div 
         tabindex="0"
         class="
-          p-[1px] 
           w-[1.6rem]  aspect-square 
           border border-primary-900 dark:border-primary-300 
           rounded-full
@@ -43,18 +40,18 @@ const isDarkMode = useDark({ ...props?.options, disableTransition: false });
           <div 
             class="
               absolute 
-              transition duration-300 ease-in-out  
+              transition-all duration-300 ease-in-out  
               h-full w-[200%] 
               rounded-full
-              flex items-center justify-between
-              px-[30%]
+              flex items-center gap-3
+              px-[27%]
               select-none
               text-[0.8rem] font-bold
               text-primary-900 dark:text-primary-100
               bg-primary-200 dark:bg-primary-800 
               shadow 
             "
-            :class="`${ isDarkMode ? '-translate-x-1/2' : '' }`"
+            :class="`${ isDarkMode ? '-left-full' : 'left-0' }`"
           >
             <!-- Sun -->
             <p>
