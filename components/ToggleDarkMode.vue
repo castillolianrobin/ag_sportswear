33b7f6ff79c875aa5f62b63ddb15d6dcf36f4271
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-06-26 21:45:59 +0800

    Style: Change dark mode toggle

--- components/ToggleDarkMode.vue
+++ components/ToggleDarkMode.vue
@@ -26,11 +26,8 @@ const isDarkMode = useDark({ ...props?.options, disableTransition: false });
         tabindex="0"
         class="
           w-[1.6rem]  aspect-square 
-          border border-primary-900 dark:border-primary-300 
           rounded-full
-          focus:outline outline-1 outline-primary-600 dark:outline-primary-300
-          bg-white/25
-          dark:bg-black/25
+          focus:outline-0 outline-1 outline-primary-600 dark:outline-primary-300
           transition
           overflow-hidden
         "
@@ -41,14 +38,13 @@ const isDarkMode = useDark({ ...props?.options, disableTransition: false });
             class="
               absolute 
               transition-all duration-300 ease-in-out  
-              h-full w-[200%] 
+              h-full w-[200%]
               rounded-full
-              flex items-center gap-3
-              px-[27%]
+              flex items-center gap-2 md:gap-1.5
+              px-[20%] md:px-[10%]
               select-none
-              text-[0.8rem] font-bold
-              text-primary-900 dark:text-primary-100
-              bg-secondary-100 dark:bg-secondary-800 
+              text-lg md:text-2xl font-bold
+              text-primary-200 dark:text-primary-100
               shadow 
             "
             :class="`${ isDarkMode ? '-left-full' : 'left-0' }`"
